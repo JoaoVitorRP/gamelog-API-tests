860b7dc069e8f083618b0fc6682aff4db228463a
JoaoVRP <joaovrp.mec.ufsc@gmail.com>
2023-01-20 20:50:31 -0300

    refact: changed dbschema to simplify the project

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import genresRouter from "./routes/genres.route.js";
-import reviewersRouter from "./routes/reviewers.route.js";
 import gamesRouter from "./routes/games.route.js";
 
 dotenv.config();
@@ -11,7 +10,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(genresRouter);
-app.use(reviewersRouter);
 app.use(gamesRouter);
 
 const port = process.env.PORT || 5000;
